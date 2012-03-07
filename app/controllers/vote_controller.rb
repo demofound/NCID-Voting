a6680b363410fcc58dfd4a616e75cfd4fb3f6184
Dan Connor <danconn@danconnor.com>
2012-03-06 18:20:14 -0800

    Creating vote controller and routes.

---
+++ app/controllers/vote_controller.rb
@@ -0,0 +1,9 @@
+class VoteController < ApplicationController
+  # renders the page where a voter will decide what vote to cast
+  def new
+  end
+
+  # handles the submission of a casted voted by a voter
+  def cast
+  end
+end

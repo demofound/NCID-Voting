29bd909ca61f8a6a4567612f1eacaca0b4edb551
Dan Connor <danconn@danconnor.com>
2012-03-09 15:21:55 -0800

    Including 'view hash' helpers

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,9 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+# include hash helpers:
+#  in general I don't like sending 'live' activerecord objects to views, so
+#  I have created these hash generators to create simple hashes for use in views
+Dir.glob(CCROOT / 'lib/nci/views/*.rb') {|lib| require lib }
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])

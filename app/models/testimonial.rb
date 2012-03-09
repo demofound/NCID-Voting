ce9051a8c21cdc2c76f37ad041cbae08ed4b6534
Dan Connor <danconn@danconnor.com>
2012-03-09 15:21:32 -0800

    Adding recent testimonial helper

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -4,6 +4,10 @@ class Testimonial < ActiveRecord::Base
   before_save :set_state_default
   enum_attr :state, %w(unapproved approved featured)
 
+  def self.recent_featured(count = 3)
+    return self.where(:state => :featured).order("created_at DESC").limit(count)
+  end
+
   private
 
   # can't seem to find a :default option for enumerated_attribute so this will have to do

1981f2f8926e45d67624dc810ed48276213fbfdd
Dan Connor <danconn@danconnor.com>
2012-03-09 21:49:16 -0800

    Fixing up vote routes so they make a little more sense.

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ NciVote::Application.routes.draw do
 
   devise_for :users, ActiveAdmin::Devise.config
 
-  controller :vote, :path => "/:initiative_code/vote" do
-    get    "/cast"      => :new,    :as => :new_vote
-    post   "/cast"      => :create, :as => :create_vote
-    get    "/:ref_code" => :show,   :as => :show_vote
-    put    "/:ref_code" => :update, :as => :update_vote
-    delete "/:ref_code" => :delete, :as => :delete_vote
+  controller :vote, :path => "/" do
+    get    ":initiative_code/vote"  => :new,    :as => :new_vote
+    post   ":initiative_code/vote"  => :create, :as => :create_vote
+    get    "vote/:ref_code"         => :show,   :as => :show_vote
+    put    "vote/:ref_code"         => :update, :as => :update_vote
+    delete "vote/:ref_code"         => :delete, :as => :delete_vote
   end
 end

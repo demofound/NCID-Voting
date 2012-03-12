99dc045796febf8ebf99fe4583c0969e60a69d29
Dan Connor <danconn@danconnor.com>
2012-03-11 17:43:40 -0700

    Adding an active_admin-esque layout so that the registration forms are similar in appearance. Also changing over those forms to use formtastic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "active_admin_esque" # we emulate the active_admin layout for consistancy
+    else
+      "application"
+    end
+  end
 end

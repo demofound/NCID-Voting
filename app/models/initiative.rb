f1b92427328c8d0901ac1cdb5d42f98cf09b7a2c
Dan Connor <danconn@danconnor.com>
2012-03-11 20:29:10 -0700

    Fixing initiative relationship to user

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -1,5 +1,5 @@
 class Initiative < ActiveRecord::Base
-  has_one  :creator, :class_name => "User", :foreign_key => "creator_id"
+  belongs_to  :creator, :class_name => "User", :foreign_key => "user_id"
   has_many :votes
 
   before_save       :set_defaults

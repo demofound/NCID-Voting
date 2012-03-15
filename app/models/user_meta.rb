cfc8e1715c109daee18ac79438c59f91f15b1deb
Dan Connor <danconn@danconnor.com>
2012-03-14 20:42:01 -0700

    Fixing encryption key

--- app/models/user_meta.rb
+++ app/models/user_meta.rb
@@ -2,11 +2,12 @@ class UserMeta < ActiveRecord::Base
   belongs_to :user
   belongs_to :state
 
-  attr_encrypted :ssn,     :key => 'a secret key', :algorithm => ATTR_ENCRYPTED_CIPHER
-  attr_encrypted :address, :key => 'a secret key', :algorithm => ATTR_ENCRYPTED_CIPHER
+  attr_encrypted :ssn,     :key => ATTR_ENCRYPTED_KEY, :algorithm => ATTR_ENCRYPTED_CIPHER
+  attr_encrypted :address, :key => ATTR_ENCRYPTED_KEY, :algorithm => ATTR_ENCRYPTED_CIPHER
 
   # if we have an associated state and if that associated state requires the fields in question
-  validates_presence_of :ssn,     :if => Proc.new { |um| um.state_id && um.state.required_fields.include?(:ssn_last_four)}
-  validates_presence_of :address, :if => Proc.new { |um| um.state_id && um.state.required_fields.include?(:address) }
-  validates_presence_of :user_id
+  validates_presence_of   :ssn,     :if => Proc.new { |um| um.state_id && um.state.required_fields.include?(:ssn_last_four)}
+  validates_presence_of   :address, :if => Proc.new { |um| um.state_id && um.state.required_fields.include?(:address) }
+  validates_presence_of   :user_id
+  validates_uniqueness_of :user_id
 end
